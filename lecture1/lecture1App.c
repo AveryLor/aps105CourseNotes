a2afc0412d184edaec89a69fdffb02a3bdaa7c4c
Avery Lor <averywlor@gmail.com>
2025-01-06 11:52:21 -0500

    Added explanation comments

--- lecture1/lecture1App.c
+++ lecture1/lecture1App.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h> // This is the standard input output file that allows the program to print to the console
 
-int main() {
+int main() { // The program starts in main, it finds the main function and runs that function
 
     int integer =  10; 
 
-    printf("Hello, World!\n");
+    printf("Hello, World!\n"); // This is a function that prints to the console
     printf("%d", integer);
-    return 0; 
+    return 0; // Will print 0 if there is no error, otherwise it will print a non-zero number
 }
