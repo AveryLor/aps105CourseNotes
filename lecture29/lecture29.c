34d642741142a95ca4fb7c2469a7bb227183a348
Avery Lor <averywlor@gmail.com>
2025-03-17 22:43:46 -0400

    done notes

---
+++ lecture29/lecture29.c
@@ -0,0 +1,11 @@
+// A struct is a user-defined data type that allows you to group together variables of different types.
+
+#include <stdio.h> 
+
+int main() {
+    struct Person { // This is an example of a struct
+        char name[50];   
+        int age;
+    };
+    // -> This is how you declare a variable of the struct type
+}
---
+++ lecture30/linkedList.c
@@ -0,0 +1,3 @@
+//  A linear data structure where elements (nodes) are linked using pointers. 
+
+// 
